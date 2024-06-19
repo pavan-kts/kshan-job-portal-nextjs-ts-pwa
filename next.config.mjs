b376eaec25ac643820ebe1afe3ee75bf42d7fde3
“Pavan <pavan@kshantechsoft.com>
2024-06-19 12:14:46 +0530

    hamburger shifts to right

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,19 @@
-import { default as withPWA } from 'next-pwa';
+/** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+import withPWA from 'next-pwa';
+
+// const isDevelopment = process.env.NODE_ENV === 'development';
 
 const pwaConfig = {
   dest: 'public',
-  // disable: process.env.NODE_ENV === 'development',
+//   disable: isDevelopment, // Disable PWA in development mode
 };
 
 const nextConfig = {
   reactStrictMode: true,
+  // other Next.js configuration options
 };
 
 export default withPWA(pwaConfig)(nextConfig);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,25 +28,26 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <div className={`bg-white dark:bg-gray-900`}>
+    <div className={`bg-white dark:bg-gray-900 px-2`}>
       <div className="container mx-auto py-2 flex flex-col lg:flex-row justify-between items-center">
-        {/* Logo */}
-        <Link href="/" className="flex items-center space-x-2">
-          <img
-            src={isDarkMode ? "/assets/img/Kshan_tech_soft_menu_white.png" : "./assets/img/kts-logo.png"}
-            alt="Logo"
-            className="w-56 h-auto my-5"
-          />
-        </Link>
+        {/* Logo and Hamburger Icon for Mobile */}
+        <div className="flex items-center justify-between w-full">
+          <Link href="/" className="flex items-center space-x-2">
+            <img
+              src={isDarkMode ? "/assets/img/Kshan_tech_soft_menu_white.png" : "./assets/img/kts-logo.png"}
+              alt="Logo"
+              className="w-56 h-auto my-5"
+            />
+          </Link>
 
-        {/* Hamburger Icon for Mobile */}
-        <div className="block lg:hidden">
-          <button
-            onClick={toggleMenu}
-            className="text-gray-600 dark:text-white focus:outline-none"
-          >
-            <FaBars className="w-6 h-6" />
-          </button>
+          <div className="block lg:hidden">
+            <button
+              onClick={toggleMenu}
+              className="text-gray-600 dark:text-white focus:outline-none"
+            >
+              <FaBars className="w-6 h-6" />
+            </button>
+          </div>
         </div>
 
         {/* Menu Links */}
@@ -57,17 +58,17 @@ const Navbar: React.FC = () => {
             <NavItem href="/job" label="Job" />
             <NavItem href="/training" label="Training" />
             <NavItem href="/events" label="Events" />
-            <NavItem href="/contact-us" label="Contact us" />
-            <NavItem href="/about-us" label="About us" />
+            <NavItem href="/contact-us" label="Contact" />
+            <NavItem href="/about-us" label="About" />
           </div>
         </div>
 
         {/* Sign Up and Sign In Buttons */}
         <div className="hidden lg:flex space-x-2">
-          <Link href={"/signup"} className="cursor-pointer text-black dark:text-white text-base font-normal px-4 py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700">
+          <Link href={"/signup"} className="cursor-pointer text-black dark:text-white w-28 text-base text-center font-normal py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700">
             Sign up
           </Link>
-          <Link href={"/signin"} className="bg-blue-500 dark:bg-gray-700 rounded-lg w-22 cursor-pointer text-white text-base font-normal px-4 py-2 hover:bg-gray-800 dark:hover:bg-gray-600">
+          <Link href={"/signin"} className="bg-blue-500 dark:bg-gray-700 rounded-lg w-28 cursor-pointer text-white text-base text-center font-normal py-2 hover:bg-gray-800 dark:hover:bg-gray-600">
             Sign In
           </Link>
         </div>
