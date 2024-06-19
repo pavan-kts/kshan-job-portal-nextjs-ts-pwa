72bbfe08850b3e2ce24a306a8fe4e98134ebc1dc
“Pavan <pavan@kshantechsoft.com>
2024-06-19 09:50:09 +0530

    config update

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import { default as withPWA } from 'next-pwa';
 
 const pwaConfig = {
   dest: 'public',
-//   disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development',
 };
 
 const nextConfig = {
